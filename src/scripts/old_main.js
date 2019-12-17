b74207a4641b47fbe2aa0915135a2bbb7074c862
leofuster <fusterleo@me.com>
2019-12-17 23:44:54 +0100

    Initial commit

---
+++ src/scripts/old_main.js
@@ -0,0 +1,13 @@
+// import { GetBacon } from './utils';
+// import './EffectShell';
+// import './StretchEffect';
+/* IMPORTER TOUS LES FICHIERS */
+
+// const baconEl = document.querySelector('.bacon');
+
+// GetBacon()
+//   .then(res => {
+//     const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
+//     baconEl.innerHTML = markup;
+//   })
+//   .catch(err => (baconEl.innerHTML = err));
